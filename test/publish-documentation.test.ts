a6269678b3a02f785f9424d913ee79784afea4eb
Artem Ufimtcev <artem@supernova.io>
2024-08-08 17:01:45 +0200

    Rework tests

--- test/publish-documentation.test.ts
+++ test/publish-documentation.test.ts
@@ -9,25 +9,24 @@
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Imports
 
-import { test } from "@oclif/test"
+import { runCommand } from "@oclif/test"
+import { expect } from "chai";
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Tests
 
 describe("publish-documentation", function () {
-  this.timeout(10000)
-  const commandAttributes = [
-    "publish-documentation",
-    `--apiKey=${process.env.TEST_API_KEY}`,
-    `--designSystemId=${process.env.TEST_DESIGN_SYSTEM_ID}`,
-    `--target=${process.env.TEST_DOC_ENVIRONMENT}`,
-    `--environment=${process.env.TEST_ENVIRONMENT}`,
-  ]
+  it("should run command", async function() {
+    this.timeout(10000)
+    const commandAttributes = [
+      "publish-documentation",
+      `--apiKey=${process.env.TEST_API_KEY}`,
+      `--designSystemId=${process.env.TEST_DESIGN_SYSTEM_ID}`,
+      `--target=${process.env.TEST_DOC_ENVIRONMENT}`,
+      `--environment=${process.env.TEST_ENVIRONMENT}`,
+    ]
 
-  test
-    .do((ctx) => {
-      console.log(commandAttributes.join(" \\\n  "))
-    })
-    .command(commandAttributes)
-    .it()
+    const result = await runCommand(commandAttributes);
+    expect(result.error).to.be.undefined;
+  })
 })
--- test/sync-tokens.test.ts
+++ test/sync-tokens.test.ts
@@ -9,27 +9,26 @@
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Imports
 
-import { expect, test } from "@oclif/test"
+import { runCommand } from "@oclif/test"
+import { expect } from "chai"
 import * as path from "path"
 
 // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 // MARK: - Tests
 
 describe("sync-tokens-single-file", function () {
-  this.timeout(30000)
-  const commandAttributes = [
-    "sync-tokens",
-    `--apiKey=${process.env.TEST_API_KEY}`,
-    `--designSystemId=${process.env.TEST_SYNC_DESIGN_SYSTEM_ID}`,
-    `--tokenFilePath=${path.join(process.cwd(), "test-resources", "figma-tokens", "single-file-sync", "tokens.json")}`,
-    `--configFilePath=${path.join(process.cwd(), "test-resources", "figma-tokens", "single-file-sync", "supernova.settings.json")}`,
-    `--environment=${process.env.TEST_ENVIRONMENT}`,
-  ]
+  it("should run command", async function () {
+    this.timeout(30000)
+    const commandAttributes = [
+      "sync-tokens",
+      `--apiKey=${process.env.TEST_API_KEY}`,
+      `--designSystemId=${process.env.TEST_SYNC_DESIGN_SYSTEM_ID}`,
+      `--tokenFilePath=${path.join(process.cwd(), "test-resources", "figma-tokens", "single-file-sync", "tokens.json")}`,
+      `--configFilePath=${path.join(process.cwd(), "test-resources", "figma-tokens", "single-file-sync", "supernova.settings.json")}`,
+      `--environment=${process.env.TEST_ENVIRONMENT}`,
+    ]
 
-  test
-    .do((ctx) => {
-      console.log(commandAttributes.join(" \\\n  "))
-    })
-    .command(commandAttributes)
-    .it()
+    const result = await runCommand(commandAttributes);
+    expect(result.error).to.be.undefined;
+  })
 })
